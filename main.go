1bdc221c2ef01a225b2e9906b6fc6d5a55f0abc6
Sean <sean.yang@econommi.io>
2023-05-04 11:54:51 -0700

    created home and about page with addValues function

--- main.go
+++ main.go
@@ -5,16 +5,41 @@ import (
 	"net/http"
 )
 
+var portNumber = ":8080"
+
+// in order for a function to respond to a request from a web browser it needs two parameters
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	// fmt.Printf(w, "This is the about page")
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+}
+
+// if your function name starts with a capital letter, it means you can cann it from outside the package execpt
+// except the main package
+// having it lowercase makes the function private
+func addValues(x, y int) int {
+	return x + y
+}
+
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, world!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	n, err := fmt.Fprintf(w, "Hello, world!")
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// 	fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	// })
 
-	http.ListenAndServe(":8080", nil)
+	// ---------
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 
 }
